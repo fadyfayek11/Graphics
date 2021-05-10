86eedc6b65638ffadde3e3be8dcd6ab06c70e4c0
fadyfayek11 <fayekfady24@gmail.com>
2021-05-10 15:12:39 +0200

    Main

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,26 +10,20 @@ namespace Graphics
         public static void Main(string[] args)
         {
             //DDA dDA = new DDA();
-            //dDA.DDAline(0, 0, 4, 6);
+            //dDA.DDAline(2, 3, 9, 8);
 
-            //for (int i = 0; i < dDA.Xpoints.Count; i++)
-            //{
-            //    Console.WriteLine(dDA.Xpoints[i] + "\t" + dDA.Ypoints[i]);
-            //}
 
-            //Bresenham bresenHam = new Bresenham();
-            //bresenHam.Bresline(32, 3, 48, 8);
-            //for (int i = 0; i < bresenHam.Xpoints.Count; i++)
-            //{
-            //    Console.WriteLine(bresenHam.Xpoints[i] + "\t" + bresenHam.Ypoints[i]);
-            //}
+            Bresenham bresenHam = new Bresenham();
+            bresenHam.Bresline(20, 10, 30, 18);
 
-            Circle circle = new Circle();
-            circle.CirclePoints(10);
-            for (int i = 0; i < circle.Xpoints.Count; i++)
-            {
-                Console.WriteLine(circle.Xpoints[i] + "\t" + circle.Ypoints[i]);
-            }
+
+            //Circle circle = new Circle();
+            //circle.CirclePoints(10);
+
+            //Ellipse ellipse = new Ellipse();
+
+            //ellipse.EllipsPoints(8, 6);
+
 
         }
 
